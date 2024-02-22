518d013189b580ef61c730989a0d459736f0c896
khassarMohamed <mohamedkhassar775@gmail.com>
2024-02-22 16:01:48 +0100

    Update codebase to remove unused CSS utility function and update homepage text and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,11 +13,11 @@ const Index = () => {
         <div>
             <div className='flex justify-center items-center h-screen bg-cover bg-center bg-[url(/assets/img/innovate.jpg)]' data-aos='fade-down'>
                 <div className='backdrop-blur-sm bg-black/5 h-full w-full flex justify-center items-center'>
-                    <h1 className='text-5xl font-semibold text-white'>Welcome to Tech Innovate</h1>
+                    <h1 className='lg:text-5xl text-3xl font-semibold text-white text-center'>Welcome to Tech Innovate</h1>
                 </div>
             </div>
             <div className='m-12 grid gap-y-10 justify-center text-white'>
-                <h1 className='text-center text-4xl'>🚀 Discover Innovation</h1>
+                <h1 className='text-center lg:text-4xl text-[1.65rem] text-nowrap'>🚀 Discover Innovation</h1>
                 <p className='w-full text-justify'>Embark on a journey into a realm where imagination transcends into reality. At Tech Innovate, we are catalysts of progress, constantly challenging the confines of possibility through unyielding creativity. Our ethos is steeped in the belief that innovation knows no bounds. Within our ecosystem, we foster an environment where audacious ideas flourish and groundbreaking solutions emerge. From Artificial Intelligence and Machine Learning, reshaping the landscape of cognition, to IoT and Connected Devices, crafting intelligent networks that elevate everyday experiences, our expertise spans the forefront of technological evolution. Delve into the world of Blockchain and Cryptocurrencies, where we redefine the standards of security and trust, revolutionizing financial transactions and beyond. Moreover, in the realm of Biotechnology and Healthcare, we pioneer transformation medical advancements, nurturing a healthier, more resilient future for all. Join us in our unwavering pursuit of innovation; together, we transcend limitations and shape a future limited only by the scope of our imagination.</p>
             </div>
         </div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,30 @@
-import { BrainCircuit } from 'lucide-react'
+import { BrainCircuit, Menu, X } from 'lucide-react'
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const NavBar = () => {
+    const [isDisplay, setIsDisplay] = useState(false)
     return (
-        <header className='sticky top-0 z-10 font-bold p-5 grid grid-cols-2 gap-x-8 items-center text-slate-500 backdrop-blur-lg bg-black/5 w-full justify-start'>
-            <div className='flex justify-center items-center gap-x-5'>
-                <BrainCircuit size={50} />
-                <h1 className='text-2xl'>Tech Innovate</h1>
+        <header className='fixed left-0 top-0 z-10 font-bold lg:p-5 p-4 lg:flex justify-center lg:gap-x-8 items-center text-white lg:text-slate-500 backdrop-blur-lg bg-black/5 lg:justify-between w-full'>
+            <div className='lg:hidden float-end' onClick={() => setIsDisplay(!isDisplay)}>
+                {isDisplay ? <X /> : <Menu />}
             </div>
-            <nav>
-                <ul className='grid grid-cols-4 gap-x-8 text-md capitalize justify-start'>
-                    <Link href="/">
+            <div className='flex justify-center items-center lg:gap-x-5'>
+                <BrainCircuit className='hidden lg:block' />
+                <h1 className='text-2xl text-nowrap'>Tech Innovate</h1>
+            </div>
+            <nav className={`${isDisplay ? "block" : "hidden"} "lg:block hidden"`}>
+                <ul className='lg:grid lg:grid-cols-4 lg:gap-x-8 text-md capitalize flex flex-col justify-center items-center gap-6 h-screen'>
+                    <Link href="/" onClick={() => setIsDisplay(false)}>
                         <li>home</li>
                     </Link>
-                    <Link href="/services">
+                    <Link href="/services" onClick={() => setIsDisplay(false)}>
                         <li>services</li>
                     </Link>
-                    <Link href="/portfolio">
+                    <Link href="/portfolio" onClick={() => setIsDisplay(false)}>
                         <li>portfolio</li>
                     </Link>
-                    <Link href="/contact">
+                    <Link href="/contact" onClick={() => setIsDisplay(false)}>
                         <li>contact</li>
                     </Link>
                 </ul>
